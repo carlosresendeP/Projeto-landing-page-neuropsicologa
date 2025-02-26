8ba4bee41ddbdf197daa7b79f694f07d7d1500b0
carlosresendeP <carlosresende.paula@gmail.com>
2025-02-25 21:44:14 -0300

    website upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const button = document.querySelector(".menu-button")
+const sideBar = document.querySelector(".navbar-mobile")
+const background = document.querySelector(".background")
+
+button.addEventListener("click", ()=>{
+    // Verifica se a sidebar está ativa
+    const isActive = button.classList.contains("active")
+
+    // Adiciona ou remove a classe active
+    button.classList.toggle("active", !isActive);
+    sideBar.classList.toggle("active", !isActive);
+    background.classList.toggle("active", !isActive);
+
+    button.innerHTML = isActive ? "<i class='fa-solid fa-bars' ></i>" : "X";
+
+})
+
+background.addEventListener("click", () => {
+    // Remove as classes 'active' se a sidebar estiver ativa
+    if (sideBar.classList.contains("active")) {
+        button.classList.remove("active");
+        sideBar.classList.remove("active");
+        background.classList.remove("active");
+        button.innerHTML = "<i class='fa-solid fa-bars' ></i>"; // Reseta o texto do botão
+    }
+});
+
+
+//data exata do ano
+const yeardate = document.querySelector(".date")
+
+yeardate.innerHTML = new Date().getFullYear()
